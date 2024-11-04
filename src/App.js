9ee01dffc6d093987727d376a33e93f4b869000d
Durgalakshmi <durgalakshmi.jaya@aspiresys.com>
2024-11-04 17:58:49 +0530

    React Explorations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 // import Counter from './AE/Sample';
-
+import Arrays from './AE/Arrays';
+import Functions from './AE/Functions';
+import Objects from './AE/Objects';
+import JSONString from './AE/JsonString';
+import Asynchronous from './AE/Asynchronous';
+import Intro from './AE/React/Intro';
+import Welcome from './AE/React/Rendering';
+import Counter from './AE/React/StatesAndProps';
+import Timer from './AE/React/LifeCycle';
+import EventHandler from './AE/React/Handlers';
+import Num from './AE/React/ConditionalRendering';
+import NameList from './AE/React/ListAndKeys';
+import Forms from './AE/React/Forms';
+import VotingApp from './AE/React/LiftingStateUp';
+import PageLayout from './AE/React/Composition';
+import Inheritance from './AE/React/Inheritance';
+import Hooks from './AE/React/Hooks';
+import SemanticHTML from './AE/React/SemanticHTML';
+import Navigation from './AE/React/KeyboardNav';
+import Modal from './AE/React/ManagingFocus';
+import CodeSplit from './AE/React/CodeSpliting';
+import ThemeProvider from './AE/React/ThemeContext';
+import ThemedComponent from './AE/React/ThemedComponent';
+import ErrorBoundary from './AE/React/ErrorBoundary';
+import ErrorComponent from './AE/React/Component';
+import Table from './AE/React/Frangments';
+// import ChartIntegration from './AE/React/Library';
+import JSXinDeep from './AE/React/JSX';
+import ProfilerExp from './AE/React/Profiler';
+import Es6 from './AE/React/ReactWithoutEs6';
+import WithoutJSX from './AE/React/withoutJSX';
+import ReduxImp from './AE/React/Redux';
+import TypescriptExp from './AE/React/TypescriptExp';
+import DataFetching from './AE/React/Axios';
+import Display from './AE/React/Dispay';
+import DataList from './AE/React/Redux/Display';
 import LoginMain from './AE/React/LoginMain';
 
 function App() {
